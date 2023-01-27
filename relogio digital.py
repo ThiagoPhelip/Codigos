45674b821663cee2197e4ad25772715014f735de
Thiago Phelip da Silva Cunha <83424010+ThiagoPhelip@users.noreply.github.com>
2023-01-27 10:44:44 -0300

    Create relogio digital.py

---
+++ relogio digital.py
@@ -0,0 +1,10 @@
+import time
+
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    print(current_time)
+    time.sleep(1)
+    
+    ## Este código usa a biblioteca "time" para obter a hora atual e imprimi-la na tela no formato "HH:MM:SS". A função "sleep(1)" faz com que o código pare por um segundo antes de imprimir a hora novamente, criando assim a sensação de um relógio digital atualizando a cada segundo.
+
+## Você pode personalizar esse código adicionando mais funções, por exemplo, usando biblioteca Tkinter para criar uma interface gráfica para o relógio digital.##
